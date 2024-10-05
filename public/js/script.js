f2354357f32da27e140e39db397a9f42a5959108
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-10-05 17:40:04 +0700

    2.1. Thêm tour vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -9,3 +9,38 @@ if(tourImages){
     });
 }
 // End tour-images
+
+// Giỏ hàng
+const cart = localStorage.getItem("cart");
+if(!cart){
+    localStorage.setItem("cart", JSON.stringify([]));
+}
+
+const formAddToCart = document.querySelector("[form-add-to-cart]");
+if(formAddToCart){
+    formAddToCart.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const tourId = parseInt(formAddToCart.getAttribute("tour-id"));
+        const quantity = parseInt(formAddToCart.quantity.value);
+
+        if(tourId && quantity > 0){
+            const cart = JSON.parse(localStorage.getItem("cart"));
+            
+            const existTour = cart.find(item => item.tourId == tourId);
+
+            if(existTour){
+                existTour.quantity = existTour.quantity + quantity;
+            }
+            else{
+                cart.push({
+                    tourId: tourId,
+                    quantity: quantity
+                });
+            }
+
+            localStorage.setItem("cart", JSON.stringify(cart));
+        }
+    });
+}
+// Hết Giỏ hàng
