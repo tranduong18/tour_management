b91c93540ce9679707d3ed458ec1fc213499fe61
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-10-06 07:28:18 +0700

    3.1. Làm giao diện

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,9 +1,12 @@
 import { Express } from "express";
 import { tourRoute } from "./tour.route";
 import { categoryRoute } from "./category.route";
+import { cartRoute } from "./cart.route";
 
 export const routesClient = (app: Express) => {
     app.use("/tours", tourRoute);
 
     app.use("/categories", categoryRoute);
+
+    app.use("/cart", cartRoute);
 }
