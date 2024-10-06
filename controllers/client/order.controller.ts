a1c58ff649310c56bc8ab52381e357ed9f540a91
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-10-06 11:17:44 +0700

    2.2. Server lấy data lưu vào bảng orders

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,11 +1,36 @@
 import { Request, Response } from "express";
+import Order from "../../models/order.model";
+import { generateOrderCode } from "../../helpers/generate.helper";
 
 // [POST] /order
 export const index = async (req: Request, res: Response) => {
-    console.log(req.body);
+    const data = req.body;
+
+    // Lưu data vào bảng orders
+    const dataOrders = {
+        code: "",
+        fullName: data.info.fullName,
+        phone: data.info.phone,
+        note: data.info.note,
+        status: "initial"
+    };
+
+    const order = await Order.create(dataOrders);
+    const orderId = order.dataValues.id;
+    const code = generateOrderCode(orderId);
+
+    await Order.update({
+        code: code
+    }, {
+        where: {
+            id: orderId
+        }
+    });
+    // Hết Lưu data vào bảng orders
 
     res.json({
         code: 200,
-        message: "Đặt hàng thành công!"
+        message: "Đặt hàng thành công!",
+        orderCode: code
     });
 }
