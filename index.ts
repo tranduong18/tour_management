142020ecac103bb02a1586c3079b8064327cfc62
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-12-11 16:14:07 +0700

    fix bug

--- index.ts
+++ index.ts
@@ -16,10 +16,10 @@ import { systemConfig } from "./config/system";
 const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Flash
 app.use(cookieParser('HHKALKS'));
